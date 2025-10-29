c336703555d6761dbbacd13be479f12a8ad4a8d9
josescobargit <jose.escobar@uisek.edu.ec>
2025-10-29 12:27:47 -0500

    Laboratorio 4 José Escobar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,11 +16,12 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
+        // Configurar la variable de entorno desde .env
         val envFile = rootProject.file(".env")
         val githubToken = if (envFile.exists()) {
             envFile.readLines()
-                .firstOrNull { it.startsWith("GITHUB_API_TOKEN:TOKEN") }
-                ?.substringAfter("GITHUB_API_TOKEN")
+                .firstOrNull { it.trim().startsWith("GITHUB_API_TOKEN=") }
+                ?.substringAfter("GITHUB_API_TOKEN=")
                 ?.trim()
                 ?: ""
         } else {
@@ -43,6 +44,7 @@ android {
         viewBinding = true
         buildConfig = true
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -62,8 +64,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+// Retrofit para networking
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
+// Convertidor Gson para serializar/deserializar JSON
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+// (Opcional pero recomendado) Interceptor de logs para depurar las llamadas de red
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+// Glide para cargar imágenes desde URLs
     implementation("com.github.bumptech.glide:glide:4.16.0")
 }
