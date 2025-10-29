c44cac4a6c489115833be90426878fb6bbc02c2b
josescobargit <jose.escobar@uisek.edu.ec>
2025-10-29 12:03:28 -0500

    Laboratorio 4 José Escobar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,19 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        val envFile = rootProject.file(".env")
+        val githubToken = if (envFile.exists()) {
+            envFile.readLines()
+                .firstOrNull { it.startsWith("GITHUB_API_TOKEN:TOKEN") }
+                ?.substringAfter("GITHUB_API_TOKEN")
+                ?.trim()
+                ?: ""
+        } else {
+            ""
+        }
+
+        buildConfigField("String", "GITHUB_API_TOKEN", "\"$githubToken\"")
     }
 
     buildTypes {
@@ -28,6 +41,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -48,4 +62,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id("co.uzzu.dotenv.gradle") version "4.0.0"
+
 }
